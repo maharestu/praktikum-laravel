01ecc4665541087bcb86cb5c7bfd2a0bc705e1db
MahaRestu <gmaharestu.05@gmail.com>
2026-09-09 09:11:37 +0700

    Praktikum minggu kedua. Route dan Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController; #Praktikum 2
 
 Route::get('/', function () {
     return view('welcome');
 });
+#=============================================================================================================
+# Praktikum 2
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
+# Tugas Praktikum 2
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+#=============================================================================================================
