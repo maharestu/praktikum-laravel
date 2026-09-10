9581e2bb28e1fbe8886d7506ca3aaf878d89b1f6
MahaRestu <gmaharestu.05@gmail.com>
2026-09-10 16:58:45 +0700

    Praktikum minggu keempat. Middleware dan login.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        #==========================================================================================================
+        # Praktikum 4
+        $middleware->alias([
+            'role' => App\Http\Middleware\CheckRole::class,
+        ]);
+        #==========================================================================================================
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
     })->create();
+    
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController; #Praktikum 2
+use App\Http\Controllers\Auth\LoginController; #Praktikum 4
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,4 +16,26 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
 Route::get('/about', function () {
     return view('about');
 })->name('about');
+#=============================================================================================================
+#Praktikum 4
+Route::get('/login', [LoginController::class, 'create'])
+    ->middleware('guest')
+    ->name('login');
+Route::post('/login', [LoginController::class, 'store'])
+    ->middleware('guest')
+    ->name('login.store');
+Route::post('/logout', [LoginController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+#Middleware CheckRole
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+    Route::get('/reports/sales', [ReportController::class, 'sales'])->name('report.sales');
+});
+
+Route::middleware(['auth', 'role:admin,kasir'])->group(function () {
+    Route::get('/pos', [PosController::class, 'index'])->name('pos.index');
+    Route::post('/pos', [PosController::class, 'store'])->name('pos.store');
+});
 #=============================================================================================================
